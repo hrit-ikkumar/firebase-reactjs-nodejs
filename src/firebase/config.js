17c342286beefeb54f59ced582315104bb6c7abc
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-17 07:37:30 +0530

    Backed as Service: Firebase has been added

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+ // Fill Your Firebase App's Configurations here :)
